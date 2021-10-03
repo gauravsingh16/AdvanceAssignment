f69affa4a22eb31349f6eb89275bde7701cb1cbc
gauravsingh16 <52907566+gauravsingh16@users.noreply.github.com>
2021-10-03 15:04:44 +0200

    added changes for link.py

--- link.py
+++ link.py
@@ -1,12 +1,10 @@
-import urllib
-<<<<<<< HEAD
+import urllib.error
 import requests
 
 def test_link(url):
 =======
 def test_link():
     url = input('Enter a URL: ')
->>>>>>> 5fe5943f684e597f57a238f2eb9c8cb436b21f4d
     try:   
         result = urllib.request.urlopen(url,None,4).getcode()
         if result == 200:
@@ -18,9 +16,3 @@ def test_link():
         return False
     except urllib.error.URLError as e:
         return e
-<<<<<<< HEAD
-
-url = input('Enter a URL: ')
-test_link(url)
-=======
->>>>>>> 5fe5943f684e597f57a238f2eb9c8cb436b21f4d
